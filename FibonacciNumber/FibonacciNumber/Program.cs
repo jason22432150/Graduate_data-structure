43b5ea7cb1af89a4221f72a433d0f3618063c0ef
jason22432150 <52876522+jason22432150@users.noreply.github.com>
2023-04-13 09:36:04 +0800

    Add project files.

---
+++ FibonacciNumber/FibonacciNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static int FibonacciRecursive(int n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
+        static int FibonacciNoRecursive(int i)
+        {
+            if (i == 0) return 0;
+            else if (i == 1) return 1;
+            else
+            {
+                int a = 0, b = 1, c = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+                return a;
+            }
+
+        }
+        static int FibonacciArray(int i)
+        {
+            int[] arr = new int[i + 1];
+            arr[0] = 0;
+            arr[1] = 1;
+            for (int j = 2; j <= i; j++)
+            {
+                arr[j] = arr[j - 1] + arr[j - 2];
+            }
+            return arr[i];
+        }
+        Console.WriteLine(FibonacciRecursive(10));
+        Console.WriteLine(FibonacciNoRecursive(10));
+        Console.WriteLine(FibonacciArray(10));
+    }
+}
---
+++ Fibonacci_Rabbit/Fibonacci_Rabbit/Program.cs
@@ -0,0 +1,16 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Enter the number of months: ");
+    int inputStr = Console.ReadLine().int();
+ 
+    Fibonacci(inputStr);
+}
+int Fibonacci(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
