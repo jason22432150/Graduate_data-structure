cfc3316ef3821bb82be22901d38f564abf9b8433
jason22432150 <nmsl22432150@gmail.com>
2023-04-14 11:35:23 +0800

    save Rabbit

--- Fibonacci_Rabbit/Fibonacci_Rabbit/Program.cs
+++ Fibonacci_Rabbit/Fibonacci_Rabbit/Program.cs
@@ -2,9 +2,29 @@
 while (true)
 {
     Console.WriteLine("Enter the number of months: ");
-    int inputStr = Console.ReadLine().int();
- 
-    Fibonacci(inputStr);
+    int inputStr = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("inputStr: " + inputStr);
+    Console.WriteLine(Fibonacci(inputStr));
+}
+int Recoursive(int input)
+{
+    return Fibonacci(input);
+}
+int Forloop(int input)
+{
+    long[] fib = new long[75];
+    int i, n;
+    fib[1] = 1;
+    fib[2] = 1;
+    if (input >= 3)
+    {
+        fib[3] = 1;
+    }
+    for (i = 3; i <= input; i++)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return 0;
 }
 int Fibonacci(int n)
 {
