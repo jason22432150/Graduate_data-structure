c770be4d47635546adac91445bc66bfe554eb3df
jason22432150 <nmsl22432150@gmail.com>
2023-04-19 19:09:19 +0800

    save

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,36 @@
+﻿class Queue
+{
+    private int[] q; // this array holds the queue
+    private int putloc, getloc; // the put and get indices
+    public Queue(int size)
+    {
+        q = new int[size]; // allocate memory for queue
+        putloc = getloc = 0;
+    }
+    public void put(int ch)
+    {
+        if (putloc == q.Length)
+        {
+            Console.WriteLine(" -- Queue is full.");
+            return;
+        }
+        q[putloc++] = ch;
+    }
+    public int get()
+    {
+        if (getloc == putloc)
+        {
+            Console.WriteLine(" -- Queue is empty.");
+            return (int)0;
+        }
+        return q[getloc++];
+    }
+    public void Display()
+    {
+        Console.WriteLine();
+    }
+}
+class Program
+{
+
+}
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,69 @@
+﻿class Stack
+{
+    private int[] stack;
+    private int top;
+    private int size;
+    public Stack(int size)
+    {
+        this.size = size;
+        stack = new int[size];
+        top = -1;
+    }
+    public void Push(int item)
+    {
+        if (top == size - 1)
+        {
+            Console.WriteLine("Stack is full");
+        }
+        else
+        {
+            top++;
+            stack[top] = item;
+        }
+    }
+    public int Pop()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack is empty");
+            return -1;
+        }
+        else
+        {
+            int item = stack[top];
+            top--;
+            return item;
+        }
+    }
+    public void Display()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            for (int i = top; i >= 0; i--)
+            {
+                Console.WriteLine(stack[i]);
+            }
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack stack = new Stack(5);
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+        stack.Push(40);
+        stack.Push(50);
+        stack.Push(60);
+        stack.Display();
+        Console.WriteLine("Poped item is {0}", stack.Pop());
+        stack.Display();
+        Console.ReadLine();
+    }
+}
