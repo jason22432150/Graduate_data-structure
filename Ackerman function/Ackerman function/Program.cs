da0e3f7821598bcda9a8eab44ef51eeca340a70a
Guo acer <nmsl22432150@gmail.com>
2023-04-24 00:01:27 +0800

    save
    save

---
+++ Ackerman function/Ackerman function/Program.cs
@@ -0,0 +1,28 @@
+﻿class ackerman
+{
+    public static int recursion(int n, int m)
+    {
+        Console.WriteLine("ackerman(" + n + "," + m + ")");
+        if (m == 0)
+            return n + 1;
+        else if (n == 0)
+            return recursion(m - 1, 1);
+        else
+            return recursion(m - 1, recursion(m, n - 1));
+        //Thread.Sleep(100);
+        Task.Delay(1000);
+    }
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Enter m : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter n : ");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine(recursion(m,n));
+            Console.ReadKey();
+        }
+    }
+}
