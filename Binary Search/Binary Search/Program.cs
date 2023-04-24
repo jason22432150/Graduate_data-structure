aeb2bc4bd1f93b3c0a05dd1749af8529a24c0a6b
Guo acer <nmsl22432150@gmail.com>
2023-04-25 01:52:46 +0800

    save
    save

--- Binary Search/Binary Search/Program.cs
+++ Binary Search/Binary Search/Program.cs
@@ -1,38 +1,38 @@
 ﻿class Program
 {
-    public static BinarySearchRecursive(int[] list, int searchnum, int left, int right)
+    public static int BinarySearchRecursive(int[] list, int searchnum, int left, int right)
     {
-        int middle;
-        if (left <= right)
-        {
-            middle = (left + right) / 2;
-            if (list[middle] == searchnum)
-            {
-                return middle;
-            }
-            else if (list[middle] >= searchnum)
-            {
-                right = middle - 1;
-                return BinarySearchRecursive(list, searchnum, left, right);
-            }
-            else if (list[middle] <= searchnum)
-            {
-                left = middle + 1;
-                return BinarySearchRecursive(list, searchnum, left, right);
-            }
-        }
-        else
+        Console.WriteLine("searchnum: {0}, left: {1}, right: {2}", searchnum, left, right);
+        if (left > right)
         {
             return -1;
         }
+        int middle = (left + right) / 2;
+        if (list[middle] == searchnum)
+        {
+            return middle;
+        }
+        else if (list[middle] > searchnum)
+        {
+            return BinarySearchRecursive(list, searchnum, left, middle - 1);
+        }
+        else if (list[middle] <= searchnum)
+        {
+            return BinarySearchRecursive(list, searchnum, middle + 1, right);
+        }
+        return -1;
     }
 
     static void Main()
     {
-        Console.WriteLine("Arrary: [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]");
-        int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        Console.Write("Enter target num : ");
-        int input = int.Parse(Console.ReadLine());
-        Console.WriteLine(BinarySearchRecursive(list, input, 0, list.Length - 1));
+        while (true)
+        {
+            Console.WriteLine("Arrary Inde: [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]");
+            Console.WriteLine("Arrary Data: [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]");
+            int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.Write("Enter target num : ");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(BinarySearchRecursive(list, input, 0, list.Length - 1));
+        }
     }
 }
