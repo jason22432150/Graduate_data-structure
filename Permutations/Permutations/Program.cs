6f2249a348f409a552ef2ae45229ab3bb2f1334c
Guo acer <nmsl22432150@gmail.com>
2023-04-26 12:23:39 +0800

    save
    save

--- Permutations/Permutations/Program.cs
+++ Permutations/Permutations/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static char[] permutations(char[] list, int i, int n)
+    {
+        int j;
+
+        if (i == n)
+        {
+            Console.Write(list[0]);
+            for (j = 1; j <= n; j++)
+            {
+                Console.Write(list[j]);
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            for (j = i; j <= n; j++)
+            {
+                Swap(ref list[i], ref list[j]);
+                permutations(list, i + 1, n);
+                Swap(ref list[i], ref list[j]);
+            }
+        }
+        return list;
+    }
+    public static Tuple<char, char> Swap(ref char i, ref char j)
+    {
+        char store = i;
+        i = j;
+        j = store;
+        return Tuple.Create(i, j);
+    }
+    static void Main()
+    {
+        char[] list = { 'a', 'b', 'c' };
+        permutations(list, 0, list.Length - 1);
+    }
+}
--- binomial coefficient/binomial coefficient/Program.cs
+++ binomial coefficient/binomial coefficient/Program.cs
@@ -1,5 +1,28 @@
-﻿class Porgram{
-    static void Main(string[] args){
-        System.Console.WriteLine("Hello World");
+﻿class Porgram
+{
+    public static int BinomialCofficiionrtRecursive(int n, int m)
+    {
+        Console.WriteLine(n + " " + m);
+        if (m == 0 || n == m)
+        {
+            return 1;
+        }
+        else
+        {
+            return (BinomialCofficiionrtRecursive(n - 1, m) + BinomialCofficiionrtRecursive(n - 1, m - 1));
+        }
+    }
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("n : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("m : ");
+            int m = int.Parse(Console.ReadLine());
+            //Console.Write("Ans: ");
+            Console.WriteLine(BinomialCofficiionrtRecursive(n, m));
+            //System.Console.WriteLine("Hello World");
+        }
     }
 }
