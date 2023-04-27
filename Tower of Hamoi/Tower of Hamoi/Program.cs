762599da93471149b3f8e9e54b4365ee19384d28
Guo acer <nmsl22432150@gmail.com>
2023-04-27 11:38:59 +0800

    Towers_of_hanio
    Towers_of_hanio

--- Tower of Hamoi/Tower of Hamoi/Program.cs
+++ Tower of Hamoi/Tower of Hamoi/Program.cs
@@ -11,11 +11,18 @@
         Console.WriteLine("Move Disk {0} from {1} to {2}", n, fromPeg, toPeg);
         Towers_of_hanio(n - 1, auxPeg, toPeg, fromPeg);
     }
-    static void Main(){
-        int n = 4; // number of disks
-        char fromPeg = 'A', toPeg = 'C', auxPeg = 'B';
 
-        Console.WriteLine("Moves required to solve the puzzle:");
-        Towers_of_hanio(n, fromPeg, toPeg, auxPeg);
+    static void Main()
+    {
+        while (true)
+        {
+            int n = 4; // number of disks
+            char fromPeg = 'A',
+                toPeg = 'C',
+                auxPeg = 'B';
+
+            Console.WriteLine("Moves required to solve the puzzle:");
+            Towers_of_hanio(n, fromPeg, toPeg, auxPeg);
+        }
     }
 }
