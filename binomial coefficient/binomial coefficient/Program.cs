ab25c6bb80a92f1b2595a9c84cbd933bcdc8c756
Guo asus <jason22432150@gmail.com>
2023-05-15 00:12:33 +0800

    save infix to postfix No1

--- binomial coefficient/binomial coefficient/Program.cs
+++ binomial coefficient/binomial coefficient/Program.cs
@@ -2,13 +2,17 @@
 {
     public static int BinomialCofficiionrtRecursive(int n, int m)
     {
-        Console.WriteLine(n + " " + m);
+        // Console.WriteLine(n + " " + m);
         if (m == 0 || n == m)
         {
+            Console.WriteLine("『{0}  ", n);
+            Console.WriteLine("  {0}』= 1", m);
             return 1;
         }
         else
         {
+            Console.WriteLine("『{0}  +『{1}  ", n - 1, n - 1);
+            Console.WriteLine("  {0}』+  {1}』", m, m - 1);
             return (BinomialCofficiionrtRecursive(n - 1, m) + BinomialCofficiionrtRecursive(n - 1, m - 1));
         }
     }
---
+++ infix_to_postfix/infix_to_postfix/Program.cs
@@ -0,0 +1,74 @@
+﻿class Program
+{
+    static int get_rank(char c)
+    {
+        switch (c)
+        {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+            case '(':
+                return 4;
+        }
+        return -1;
+    }
+    public static string infix_to_postfix(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+        string output = "";
+        foreach (char c in input)
+        {
+            if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '(')
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(c);
+                }
+                if (get_rank(stack.Peek()) < get_rank(c))
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    while (stack.Count == 0) ;
+                    {
+                        output+=stack.Pop();
+                    }
+                }
+            }
+            else if (c == ')')
+            {
+                while (stack.Count!=0)
+                {
+                    char popData = stack.Pop();
+                    output += popData;
+                    if (popData == '(')
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                output += c;
+            }
+        }
+        while (stack.Count != 0) ;
+        {
+            output += stack.Pop();
+        }
+        return output;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("input infix expression: ");
+        string input = Console.ReadLine();
+        string ans = infix_to_postfix(input);
+        Console.WriteLine(ans);
+    }
+}
