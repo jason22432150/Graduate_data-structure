1c50c6b89eda776e934afc5d15d522f016bf1000
Guo asus <jason22432150@gmail.com>
2023-05-16 15:03:56 +0800

    Finish Infix to postfix

--- infix_to_postfix/infix_to_postfix/Program.cs
+++ infix_to_postfix/infix_to_postfix/Program.cs
@@ -1,6 +1,23 @@
-﻿class program
-{
+﻿// 1. 创建一个空的操作数栈和一个空的输出队列。
 
+// 2. 从左到右扫描表达式中的每个元素。
+
+// 3. 如果当前元素是一个操作数（即数字或变量），则将其放入输出队列中。
+
+// 4. 如果当前元素是一个左括号，则将其压入操作数栈中。
+
+// 5. 如果当前元素是一个右括号，则将操作数栈中的元素弹出并放入输出队列中，直到弹出的元素是左括号为止。注意，左括号不会放入输出队列中，也不会放入操作数栈中。
+
+// 6. 如果当前元素是一个操作符，记为 `op`，则执行以下操作：
+//    a. 如果操作数栈为空，则将 `op` 压入操作数栈中。
+//    b. 如果操作数栈不为空，则比较 `op` 与操作数栈顶的操作符的优先级。如果 `op` 的优先级大于操作数栈顶的操作符，则将 `op` 压入操作数栈中。
+//    c. 否则，将操作数栈顶的操作符弹出并放入输出队列中，直到操作数栈顶的操作符优先级小于等于 `op`，然后将 `op` 压入操作数栈中。
+
+// 7. 如果表达式中的所有元素都被处理完了，则将操作数栈中的元素依次弹出并放入输出队列中，直到操作数栈为空。
+
+// 8. 输出队列中的元素就是后缀表达式。
+class program
+{
     static int OutStackPrecedence(char c)
     {
         switch (c)
@@ -40,8 +57,12 @@
     {
         Stack<char> stack = new Stack<char>();
         string postfix = "";
-        foreach (char c in infix)
+        char[] infixArray = infix.ToCharArray();
+
+        foreach (int i in infixArray)
         {
+            Console.WriteLine(i);
+            char c = (char)i;
             if (c != '+' && c != '-' && c != '/' && c != '*' && c != '^' && c != '(' && c != ')')
             {
                 postfix += c;
@@ -60,18 +81,11 @@
             }
             else if (stack.Count != 0 && OutStackPrecedence(c) <= InStackPrecedence(stack.Peek()))
             {
-                if (stack.Peek() == '^')
+                while (stack.Count != 0 && OutStackPrecedence(c) <= InStackPrecedence(stack.Peek()))
                 {
                     postfix += stack.Pop();
-                    postfix += stack.Pop();
-                    stack.Push(c);
                 }
-                else
-                {
-                    postfix += stack.Pop();
-                    stack.Push(c);
-                }
-
+                stack.Push(c);
             }
 
             else
