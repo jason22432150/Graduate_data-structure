827afa73a33c3b8492e7927b2eebfc13649517dc
Jhen- home <Nmsl22432150@gmail.com>
2023-12-29 21:16:08 +0800

    LeetCode
    增加LeetCode 題目 1. Two Sum

---
+++ LeetCode/1. Two Sum/1. Two Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/two-sum/description/
+
+namespace _1.Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] result = solution.TwoSum(nums, target);
+            for (int i = 0; i <= result.Length - 1; i++)
+            {
+                Console.WriteLine(result);
+            }
+            Console.Read();
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                for (int j = 0; j <= nums.Length - 1; j++)
+                {
+                    if (i != j && nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
