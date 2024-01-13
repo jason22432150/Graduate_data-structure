07fcf565cc4dae05007a7e07523edc7acfa508c8
Jhen- home <Nmsl22432150@gmail.com>
2024-01-13 21:01:58 +0800

    Add Two Numbers solution files

---
+++ LeetCode/Add Two Numbers/Add Two Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/add-two-numbers/
+
+namespace Add_Two_Numbers
+{
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
